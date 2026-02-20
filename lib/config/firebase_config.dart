e30f65fabb653e1be01918737bbd21a13e641c4c
PranavX500 <ps5840432@gmail.com>
2026-02-20 09:30:23 +0530

    Remove config folder from repo and ignore it

--- lib/config/firebase_config.dart
+++
@@ -1,6 +0,0 @@
-class FirebaseConfig {
-  static const String apiKey = "AIzaSyAvJo-TShqKa3cKR0-SvIIIfr8KNLYFwC4";
-
-  static String signInUrl =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$apiKey";
-}
